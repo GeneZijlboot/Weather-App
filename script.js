80c290fc57a80a97e144c6a16b36eebeae7d4f09
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-30 16:39:22 +0200

    Visuals Done

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const Country = document.getElementById('Country');
-const Humidity = document.getElementById('Humidity');
+const Wind = document.getElementById('Wind');
 const Condition = document.getElementById('Condition');
 const Temp = document.getElementById('Temp');
 const FeelsLike = document.getElementById('FeelsLike');
 const Rainchance = document.getElementById('Rainchance');
-const CheckBox = document.getElementById('checkbox');
 
 const SubmitBtn = document.getElementById('SubmitBtn');
 let UserInput = document.querySelector('#UserInput');
-const StartLocation = 'berlin';
+const StartLocation = 'Netherlands';
 
 SubmitBtn.addEventListener('click', DifferentLocation);
 
@@ -18,8 +17,6 @@ function DifferentLocation(e){
     UserInput.value = '';
 }
 
-CheckBox.addEventListener('click', RefreshLocation)
-
 //getting the API data and setting it to variables --> for CELCIUS
 function RefreshLocation(UserInput){
     fetch('https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=' + UserInput, {mode: 'cors'})
@@ -27,46 +24,26 @@ function RefreshLocation(UserInput){
     .then(function(response) {
         return response.json();
     })
-    .then(function(response){
-        if(CheckBox.checked === true){
-            console.log('test');
-            const a = response.location.country;
-            const b = response.current.humidity;
-            const c = response.current.condition.text;
-            const d = response.current.temp_f;
-            const e = response.current.feelslike_f;
-            const f = response["forecast"]["forecastday"]["0"]["day"]["daily_chance_of_rain"];
-            RenderPageF(a, b, c, d, e, f);
-        } else if(CheckBox.checked === false){
-            console.log('testbbbbbb');
-            const a = response.location.country;
-            const b = response.current.humidity;
-            const c = response.current.condition.text;
-            const d = response.current.temp_c;
-            const e = response.current.feelslike_c;
-            const f = response["forecast"]["forecastday"]["0"]["day"]["daily_chance_of_rain"];
-            RenderPageC(a, b, c, d, e, f);
-        }
+    .then(function(response) {
+        console.log(response);
+        const a = response.location.name;
+        const b = response.current.wind_kph;
+        const c = response.current.condition.text;
+        const d = response.current.temp_c;
+        const e = response.current.feelslike_c;
+        const f = response["forecast"]["forecastday"]["0"]["day"]["daily_chance_of_rain"];
+        RenderPage(a, b, c, d, e, f);
     })
 }
 
 RefreshLocation(StartLocation);
 
 //build page with API data
-function RenderPageC(country, humidity, condition, temp, feelsLike, rainchance){
-    Country.textContent = country;
-    Humidity.textContent ="Humidity: " + humidity + "%";
+function RenderPage(country, wind, condition, temp, feelsLike, rainchance){
+    Country.innerHTML = `<i class="material-icons" style="font-size:30px;color:snow">&#xe55f</i>` + country;
+    Wind.textContent = wind + "kph";
     Condition.textContent = condition;
     Temp.textContent = temp + "°C";
-    FeelsLike.textContent = "Feels like: " + feelsLike + "°C";
-    Rainchance.textContent = "Chances of Rain: " + rainchance + "%";
-}
-
-function RenderPageF(country, humidity, condition, temp, feelsLike, rainchance){
-    Country.textContent = country;
-    Humidity.textContent ="Humidity: " + humidity + "%";
-    Condition.textContent = condition;
-    Temp.textContent = temp + "F";
-    FeelsLike.textContent = "Feels like: " + feelsLike + "F";
-    Rainchance.textContent = "Chances of Rain: " + rainchance + "%";
+    FeelsLike.textContent = feelsLike + "°C";
+    Rainchance.textContent = rainchance + "%";
 }
