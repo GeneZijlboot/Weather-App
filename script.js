47cd2866f183a29a6001bbe403cc1536cc38fff8
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-31 00:48:55 +0200

    final version

--- script.js
+++ script.js
@@ -5,23 +5,19 @@ const Temp = document.getElementById('Temp');
 const FeelsLike = document.getElementById('FeelsLike');
 const Rainchance = document.getElementById('Rainchance');
 const Dates = document.getElementById('date');
-
+const TempClicky = document.getElementById('TempClicky');
 const SubmitBtn = document.getElementById('SubmitBtn');
+const Button = document.getElementById('Button');
 let UserInput = document.querySelector('#UserInput');
-const StartLocation = 'Netherlands';
+UserInput.value = "Norway"
+let Degree = '°C';
 
 SubmitBtn.addEventListener('click', DifferentLocation);
-
-function DifferentLocation(e){
-    e.preventDefault();
-    RefreshLocation(UserInput.value);
-    UserInput.value = '';
-}
+Button.addEventListener('click', TempSwap)
 
 //getting the API data and setting it to variables --> for CELCIUS
-function RefreshLocation(UserInput){
-    fetch('https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=' + UserInput, {mode: 'cors'})
-
+function RefreshLocationC(NewLocation){
+    fetch('https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=' + NewLocation, {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
@@ -41,15 +37,57 @@ function RefreshLocation(UserInput){
     })
 }
 
-RefreshLocation(StartLocation);
+//getting the API data and setting it to variables --> for FAHRENHEIT
+function RefreshLocationF(NewLocation){
+    fetch('https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=' + NewLocation, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+        const a = response.location.name;
+        const b = response.current.wind_kph;
+        const c = response.current.condition.text;
+        const d = response.current.temp_f;
+        const e = response.current.feelslike_f;
+        const f = response["forecast"]["forecastday"]["0"]["day"]["daily_chance_of_rain"];
+        const g = response.location.localtime;
+        RenderPage(a, b, c, d, e, f, g);
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+}
+
+function DifferentLocation(e){
+    e.preventDefault();
+    RefreshLocationC(UserInput.value);
+    Button.checked = false;
+    Degree = '°C';
+    TempClicky.textContent = "C";
+}
 
 //build page with API data
 function RenderPage(country, wind, condition, temp, feelsLike, rainchance, date){
     Country.innerHTML = `<i class="material-icons" style="font-size:30px;color:snow">&#xe55f</i>` + country;
     Wind.textContent = wind + "kph";
     Condition.textContent = condition;
-    Temp.textContent = temp + "°C";
-    FeelsLike.textContent = feelsLike + "°C";
+    Temp.textContent = temp + Degree;
+    FeelsLike.textContent = feelsLike + Degree;
     Rainchance.textContent = rainchance + "%";
     Dates.textContent = date;
 }
+
+function TempSwap(){
+    if(Button.checked === true){
+        TempClicky.textContent = "F";
+        Degree = '°F';
+        RefreshLocationF(UserInput.value);
+    } else if(Button.checked == false){
+        TempClicky.textContent = "C";
+        Degree = '°C';
+        RefreshLocationC(UserInput.value);
+    }
+}
+
+RefreshLocationC(UserInput.value);
